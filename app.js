72f362eb20a87dcfe59fef2ad615e3b7b6c74a29
Khamel <khamel.mumford@gmail.com>
2018-02-22 19:05:45 -0600

    Express

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(request, response) {
+    response.send('Hello world');
+    response.end();
+});
+
+app.get('/blocks', function(request, response) {
+    var blocks = ['Fixed', 'Movable', 'Rotating'];
+    response.send(blocks);
+});
+
+app.listen(3000, function() {
+        console.log('Listening on port 3000');
+    }
+
+);
